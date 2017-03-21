ab502007378396f0df73410f889d6524ff9c0210
acandee <acandee@smu.edu>
2017-03-21 14:16:56 -0500

    Update and rename temp-lambda-config.js to lambda-config.js

--- lambda-config.js
+++ lambda-config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  profile: '', // load your AWS credentials from a custom profile
+  profile: 'acandee', // load your AWS credentials from a custom profile
   region: 'us-west-2', //the region of your Lambda function
   handler: 'index.handler', //the name of the handler function: index because the main file is index.js
   role: 'arn:aws:iam::YOURACCOUNTHERE:role/lambda_basic_execution', // the Lambda role
-  functionName: '', //name
+  functionName: 'acandee', //name
   timeout: 10,
   memorySize: 128,
   publish: true, // this creates a new version of your Lambda function every time you update it
