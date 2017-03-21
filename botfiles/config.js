85fa61f6ccd9117a71e0bb366323fd8e93bce479
acandee <acandee@smu.edu>
2017-03-21 13:35:37 -0500

    Update and rename temp-config.js to config.js

---
+++ botfiles/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //Your Twitter credentials go in this file
+  consumer_key:         '9xmavYrzHHvjoGZYKzwOe602Q',
+  consumer_secret:      'xb5JGY6YXJdxWxoI9FUvQ7U8AImgux952J2w7bXUtGeH7AOUzy',
+  access_token:         '742756656614891520-etQ0YdFeo3GFCppxkDjwltdcoAm1roJ',
+  access_token_secret:  '1FQqr122pLG3tKUxZJseaVGpWGztoKaLvfGTChGmY3Vbn'
+};
